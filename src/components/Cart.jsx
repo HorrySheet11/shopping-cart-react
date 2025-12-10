32d963632d7f1e16c7a29702a8efdf6a2bd15af3
HorrySheet11 <marinnathan11@gmail.com>
2025-12-10 11:02:17 +0800

    added content for cart page
    gets cart from app.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,32 @@
+import { useOutletContext } from "react-router";
+import styles from "../styles.module.css";
+
 function Cart() {
+  const { cart } = useOutletContext();
+
+  const total = cart.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
   return (
     <div>
       <h1>Your Cart</h1>
       <p>Review the items you have added to your cart.</p>
+      <p>
+        <a href="/shop">Continue Shopping</a>
+      </p>    
+      <div className="cartDiv">
+        <ul>
+          {cart.map((product) => (
+            <li key={product.id}>
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+              <p>Price: ${product.price}</p>
+            </li>
+          ))}
+        </ul>
+        <p>Total: ${total}</p>
+      </div>
+
     </div>
   );
 }
